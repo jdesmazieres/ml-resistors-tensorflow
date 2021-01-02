a6f07177535d175aea6e558bbb2866c8cc2b2818
Jacques Desmazieres <jacques.desmazieres@gmail.com>
2021-01-02 15:52:47 +0100

    project initial setup

---
+++ src/test.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import matplotlib as mpl
+import numpy as np
+
+print("tensorflow: ", tf.__version__)
+print("matplotlib: ", mpl.__version__)
+print("numpy:      ", np.__version__)
